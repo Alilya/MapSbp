6b3d7a7cf9d9a9e23a4960120ce55f4f8171c3db
Alilya <alinakolesnikova0101@gmail.com>
2023-08-16 13:47:25 +0300

    nothing works but let it lie here

---
+++ merchant_model.js
@@ -0,0 +1,59 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'posgres',
+  host: 'localhost',
+  database: 'map',
+  password: '123456',
+  port: 8081,
+});
+
+
+export const getMerchants = () => {
+    return new Promise(function(resolve, reject) {
+      pool.query('SELECT * FROM listPlaces;', (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results.rows);
+      })
+    }) 
+  }
+
+// const getMerchants = () => {
+//     return new Promise(function(resolve, reject) {
+//       pool.query('SELECT * FROM merchants ORDER BY id ASC', (error, results) => {
+//         if (error) {
+//           reject(error)
+//         }
+//         resolve(results.rows);
+//       })
+//     }) 
+//   }
+//   const createMerchant = (body) => {
+//     return new Promise(function(resolve, reject) {
+//       const { name, email } = body
+//       pool.query('INSERT INTO merchants (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+//         if (error) {
+//           reject(error)
+//         }
+//         resolve(`A new merchant has been added added: ${results.rows[0]}`)
+//       })
+//     })
+//   }
+//   const deleteMerchant = () => {
+//     return new Promise(function(resolve, reject) {
+//       const id = parseInt(request.params.id)
+//       pool.query('DELETE FROM merchants WHERE id = $1', [id], (error, results) => {
+//         if (error) {
+//           reject(error)
+//         }
+//         resolve(`Merchant deleted with ID: ${id}`)
+//       })
+//     })
+//   }
+  
+//   module.exports = {
+//     getMerchants,
+//     createMerchant,
+//     deleteMerchant,
+//   }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+let App = ()=> {
+  const [merchants, setMerchants] = useState(false);
+  useEffect(() => {
+    getMerchant();
+  }, []);
+
+  const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'localhost:8081'
+    
+  });
+
+
+  // function createMerchant() {
+  //   let name = prompt('Enter merchant name');
+  //   let email = prompt('Enter merchant email');
+  //   fetch('http://localhost:3001/merchants', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({name, email}),
+  //   })
+  //     .then(response => {
+  //       return response.text();
+  //     })
+  //     .then(data => {
+  //       alert(data);
+  //       getMerchant();
+  //     });
+  // }
+  // function deleteMerchant() {
+  //   let id = prompt('Enter merchant id');
+  //   fetch(`http://localhost:3001/merchants/${id}`, {
+  //     method: 'DELETE',
+  //   })
+  //     .then(response => {
+  //       return response.text();
+  //     })
+  //     .then(data => {
+  //       alert(data);
+  //       getMerchant();
+  //     });
+  // }
   return (
-    <div className="App">
-        это приложение с картой и датасетом для спб
-        
+    <div>
+      {merchants ? merchants : 'There is no merchant data available'}
+      <br>
+      <button onclick={getMerchant}>get merchant</button>
+      </br>
+      //<button onclick={deleteMerchant}>Delete merchant</button>
     </div>
   );
 }
